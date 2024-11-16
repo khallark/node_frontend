976dab1fef1815f0fe225e8b1fe7e1e56a31eb1a
Kunal Khallar <khallark295@gmail.com>
2024-11-16 13:05:51 +0530

    modified:   api/index.js

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/1home.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.get('/search', (req, res) => {
