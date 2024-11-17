22999f12165381b9130b01ba5ef52f158d1a381b
Kunal Khallar <khallark295@gmail.com>
2024-11-17 13:09:23 +0530

    modified:   public/config.js

--- public/config.js
+++ public/config.js
@@ -53,9 +53,6 @@ let cat_map_str = {
 
 function disCont(section) {
     const currentPath = window.location.pathname;
-    if (currentPath === `/${section}.html`) {
-        return;
-    }
     window.location.href = `/${section}.html`;
 }
 
